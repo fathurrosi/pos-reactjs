120f8617648c96a92047f5dbe81d6031dbf365d4
Fathur Rosi <rosi.fathur@gmail.com>
2025-10-29 21:16:05 +0700

    submit in bulk checked allow read, create, update, delete and print

--- src/services/api/PrevillageService.js
+++ src/services/api/PrevillageService.js
@@ -7,39 +7,23 @@ class PrevillageService {
     this.token = localStorage.getItem('token');
   }
 
-  //GET /api/Previllage/{role}/{profile}
   async getDataList(role) {
-    // alert(this.profile);
-    // alert(this.token);
-    //const response = await axios.get(`${this.apiUrl}/${role}/${this.profile}`);
-    //GET /api/Previllage/{role}/{profile}
-    //return response.data;
-
-
-    //alert(`${this.apiUrl}/${role}/${this.profile}`);
-     const response = await axios.get(`${this.apiUrl}/${role}/${this.profile}`, {
+    const response = await axios.get(`${this.apiUrl}/${role}/${this.profile}`, {
       headers: {
         Authorization: `Bearer ${this.token}`
       }
     });
     return response.data;
-
   }
-
-  // async getDataById(id) {
-  //   const response = await axios.get(`${this.apiUrl}/{id}`);
-  //   return response.data;
-  // }
 
   async saveData(items) {
-    const response = await axios.post(this.apiUrl, items);
+    const response = await axios.post(this.apiUrl, items, {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    });
     return response.data;
   }
-
-  // async deleteData(id) {
-  //   const response = await axios.delete(`${this.apiUrl}/{id}`);
-  //   return response.data;
-  // }
 }
 
 export default PrevillageService;
